f3d180a26ae3303c012e047f74d9bb80bcd7cf4a
fexfl <fexfleischle@gmail.com>
2022-12-26 10:09:34 +0100

    Added example main function and Bugfixes

--- FFT_Functions.hh
+++ FFT_Functions.hh
@@ -1,3 +1,8 @@
+#include <iostream>
+
+// This header includes several functions that perform a Fast Fourier Transfrom from an array, a histogram, or a function, as well as the reverse transformations
+
+// Performing a FFT from a TF1 Function as the starting point:
 void fourier_from_function( TF1 *fun , double* real, double* imag){
   double xmin, xmax;
   // Set xmin and xmax to the Range values defined in the function
@@ -26,8 +31,11 @@ void fourier_from_function( TF1 *fun , double* real, double* imag){
   histo->Draw();
   // Now define a histogram to save the transformed data in
   TH1 *histo_f = 0;
+
   // Do the fourier transform
-  histo_f = histo->FFT(histo_f, "MAG");
+  // For the options, "Re" returns the real part of the transform. For other options, see TVirtualFFT class reference (try "MAG").
+  histo_f = histo->FFT(histo_f, "Re");
+  histo_f->SetTitle("Fourier transfomed histogram");
   // Data is now saved in histo_f
   // x-Axis is now the bin number and not the frequency, divide by function range to obtain frequency
   // y-Axis has to be rescaled with a factor of 1/Sqrt(n)
@@ -44,13 +52,14 @@ void fourier_from_function( TF1 *fun , double* real, double* imag){
   fft->GetPointsComplex(real, imag);
 }
 
+// Performing a FFT from a histogram with data as the starting point
 void fourier_from_histo( TH1D *histo , double* real, double* imag){
   // First create canvas for plotting
-  TCanvas *canvas = new TCanvas("canvas", "Canvas Title", 0, 0, 600, 600);
-  canvas->Divide(1,2);
+  TCanvas *canvas_histo = new TCanvas("canvas_histo", "Canvas Title", 0, 0, 600, 600);
+  canvas_histo->Divide(1,2);
 
   // Plot the original histogram
-  canvas->cd(1);
+  canvas_histo->cd(1);
   histo->Draw();
   // Define a histogram to save the transformed data in
   TH1 *histo_f = 0;
@@ -61,7 +70,7 @@ void fourier_from_histo( TH1D *histo , double* real, double* imag){
   // y-Axis has to be rescaled with a factor of 1/Sqrt(n)
 
   // Draw the Histogram:
-  canvas->cd(2);
+  canvas_histo->cd(2);
   histo_f->Draw();
 
   // Want to save data in arrays?
@@ -72,9 +81,9 @@ void fourier_from_histo( TH1D *histo , double* real, double* imag){
   fft->GetPointsComplex(real, imag);
 }
 
-void fourier_from_array(double* datapoints){
-  // Get length of the datapoints array:
-  int n = sizeof(*datapoints) / sizeof(double) ;
+// Performing a FFT from an array of real data points as a starting point
+void fourier_from_array(double* datapoints, int& n){
+  std::cout << "Datapoints: " << n << std::endl;
 
   // Create a Fourier transform object
   TVirtualFFT *fft_own = TVirtualFFT::FFT(1, &n, "R2C ES K");
@@ -88,7 +97,7 @@ void fourier_from_array(double* datapoints){
   histo_f = TH1::TransformHisto(fft_own, histo_f, "MAG");
   histo_f->SetTitle("Fourier tranformed data");
 
-  TCanvas *canvas = new TCanvas("canvas", "Canvas Title", 0, 0, 600, 600);
+  TCanvas *canvas_array = new TCanvas("canvas_array", "Canvas Title", 0, 0, 600, 600);
   histo_f->Draw();
 
   // Want to save the output data in arrays again?
@@ -96,19 +105,18 @@ void fourier_from_array(double* datapoints){
   fft_own->GetPoints(datapoints);
 }
 
-void fourier_back_from_array(double* fourierreal, double* fourierimag){
-  // Get length of the datapoints array:
-  int n = sizeof(*freqpoints_real) / sizeof(double) ;
+void fourier_back_from_array(double* fourierreal, double* fourierimag, int& n){
+  std::cout << "Datapoints: " << n << std::endl;
   
   // Create a TVirtualFFT object for the back transform
   TVirtualFFT *fft_back = TVirtualFFT::FFT(1, &n, "C2R M K");
-  fft_back->SetPointsComplex(freqpoints_real, freqpoints_imag);
+  fft_back->SetPointsComplex(fourierreal, fourierimag);
   fft_back->Transform();
 
   // Create canvas and Histogram for plotting
   TCanvas *canvas_back = new TCanvas("canvas_back", "Reverse Fourier Transform", 0, 0, 600, 600);
   TH1 *histo_back = 0;
-  histo_back = TH1::TransformHisto(fft_back, hb, "Re");
+  histo_back = TH1::TransformHisto(fft_back, histo_back, "Re");
   histo_back->Draw();
 }
 
@@ -118,7 +126,7 @@ void fourier_back_from_histo(TH1* histo_transformed){
   TH1 *histo_back = 0;
   histo_back = histo_transformed->FFT(histo_back, "Re");
 
-  TCanvas *canvas = new TCanvas("canvas", "Canvas Title", 0, 0, 600, 600);
+  TCanvas *canvas_back_histo = new TCanvas("canvas_back_histo", "Canvas Title", 0, 0, 600, 600);
   histo_back->SetTitle("Histogram after reverse fourier transform");
   histo_back->Draw();
 }
---
+++ FFT_main.cc
@@ -0,0 +1,86 @@
+#include "FFT_Functions.hh"
+#include <iostream>
+
+// This is an example main function that would use the FFT_Functions header file
+
+// These functions perform the Fourier transformations from different starting points
+// The starting arrays/histograms are generated in these functions
+void FFT_fff(); // Creates a modulated cos TF1 function and transforms it
+void FFT_ffh_modcos(); // Creates a modulated cos Histogram and transforms it (this is equivalent to the function above)
+void FFT_ffh_gauss(); // Creates a gauss-like TH1D histogram and transforms it
+
+// This is the main function, uncomment or comment the function that you want to run
+void FFT_main(){
+  //FFT_fff();
+  //FFT_ffh_gauss();
+  FFT_ffh_modcos();
+}
+
+void FFT_fff(){
+  // Test a function as an input for fourier transform:
+  int n = 1000;
+  TF1 *fmodcos = new TF1("fmodcos", "(10*cos(2*pi*5*x) + 5*cos(2*pi*40*x)) * e^(-pi*(x)^2)", -2*TMath::Pi(), 2*TMath::Pi());
+
+  // Define two arrays to store the data in
+  double real[n];
+  double imag[n];
+  
+  // Perform the FFT with function defined in header
+  fourier_from_function(fmodcos, real, imag);
+
+  // Now perform a back transform
+  fourier_back_from_array(real, imag, n);
+}
+
+void FFT_ffh_modcos(){
+  // Take the modulated cosine function from above and create a histogram from it
+  int n = 1000;
+  double xmin = -2*TMath::Pi();
+  double xmax = 2*TMath::Pi();
+  TF1 *fun = new TF1("fmodcos", "(10*cos(2*pi*5*x) + 5*cos(2*pi*40*x)) * e^(-pi*(x)^2)",xmin ,xmax );
+
+  TH1D *histo = new TH1D("histo", "Histogram from function", n, xmin, xmax);
+
+  // Fill the histogram bins with the function y-values
+  double x;
+  for (int i=0; i<n; i++){
+    // Define x value to evaluate the function at
+    x = (double) i * (xmax - xmin)/(n-1) + xmin ;
+
+    // Fill histogram with value at current x
+    histo->SetBinContent(i+1, fun->Eval(x));
+  }
+
+  // Create arrays to store the transformed data in
+  double real[n];
+  double imag[n];
+
+  // Perform the transform through the function defined in the header
+  fourier_from_histo(histo, real, imag);
+
+  // Now transform back
+  fourier_back_from_array(real, imag, n);
+}
+
+void FFT_ffh_gauss(){
+  // Test a histogram as an input for fourier transform:
+  int n = 10000;
+
+  TH1::SetDefaultSumw2();
+  gROOT->SetStyle("Plain");
+  
+  TH1D *histo = new TH1D("histo", "A Gaussian type histogram", n, -100, 100);
+
+  TRandom3 *R = new TRandom3();
+  R->SetSeed(0);
+  for(int i=0; i<n; i++){
+    histo->Fill(R->Gaus(0, 5)); 
+  }
+
+  // Create arrays to save the data in
+  double real[n];
+  double imag[n];
+
+  fourier_from_histo(histo, real, imag);
+  
+}
